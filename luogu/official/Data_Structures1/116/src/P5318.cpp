4fb30be8325d10e57a6bba08b1bff93527996798
King <kingqaquuu@outlook.com>
2024-01-09 15:42:51 +0800

    update

---
+++ luogu/official/Data_Structures1/116/src/P5318.cpp
@@ -0,0 +1,26 @@
+/*
+ * @Author: kingqaquuu
+ * @Date: 2023/12/8 14:12
+ * @FileName: P5318.cpp
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+bool visited[100010];
+struct edge{
+    int u;
+    int v
+}art[100010];
+int n,m;
+void DFS(int root){
+    if (root<n){
+
+    }
+}
+int main() {
+    scanf("%d%d",&n,&m);
+    for (int i = 1; i <= m; ++i) {
+
+    }
+    return 0;
+}
